1faf6dca08d26a5ea77f8ff7b3d0186955d94ca4
John Wang <16063639+johnwangemory@users.noreply.github.com>
2025-03-21 15:50:13 -0400

    Update MyClass.java

--- src/main/java/edu/emory/it/MyClass.java
+++ src/main/java/edu/emory/it/MyClass.java
@@ -1,1 +1,11 @@
+package edu.emory.it;
 
+public class MyClass {
+    String username = "john";
+    String password = "mysecretpassword";
+    String token = "SKJwushwih872najdskjSYawd";
+    String pwd = "SKJwushwih872najdskjSYawd";
+    String passwd = "SKJwushwih872najdskjSYawd";
+    String passphrase = "SKJwushwih872najdskjSYawd";
+    String client_secret = "SKJwushwih872najdskjSYawd";
+}
